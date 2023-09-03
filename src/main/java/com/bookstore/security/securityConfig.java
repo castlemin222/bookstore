29fd428e16665956e324b281fe13d35fea2aa07a
sungmin <castlemin222@naver.com>
2023-09-03 17:07:26 +0900

    fix:로그인 오류 수정

--- src/main/java/com/bookstore/security/securityConfig.java
+++ src/main/java/com/bookstore/security/securityConfig.java
@@ -26,10 +26,10 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
 			// CSRF 토큰 사용여부 설정
 			.csrf().disable()		// csrf 토큰 사용을 비활성화한다.(기본값은 csrf 토큰을 사용한다.)
 			// 1. 인가정책 설정 시작
-			.authorizeHttpRequests()	// 모든 요청에 대해서 아래에 설정된 인가정책을 적용하도록 한다. AuthorizationManagerRequestMatcherRegistry 객체를 반환한다.
-			.antMatchers("/", "/register", "/login").permitAll()	// 제시된 요청은 접근을 허용한다.
-			.antMatchers("/user/**").hasRole("USER")	// 제시된 요청은 ROLE_USER 권한이 필요하다.
-			.antMatchers("/admin/**").hasRole("ADMIN")	// 제시된 요청은 ROLE_ADMIN 권한이 필요하다.
+			.authorizeHttpRequests()	// 모든 요청에 대해서 아래에 설정된 인가정책을 적용하도록 한다. 
+			.antMatchers("/", "/**", "/registerForm", "/login").permitAll()	// 제시된 요청은 접근을 허용한다.
+			.antMatchers("/user/**").hasRole("USER")	// 제시된 요청은 USER 권한이 필요하다.
+			.antMatchers("/admin/**").hasRole("ADMIN")	// 제시된 요청은 ADMIN 권한이 필요하다.
 			.anyRequest().authenticated()				// 위에서 제시된 요청외의 모든 요청도 반드시 인증이 필요하다.
 			//  1. 인가정책 설정 종료
 		.and()	
@@ -50,7 +50,7 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
 			// 3. 로그아웃정책 설정 종료
 		.and()
 			// 4. 예외처리 정책 설정 시작
-			.exceptionHandling()				// 예외처리, ExceptionHandlingConfigurer객체를 반환한다.
+			.exceptionHandling()				
 			.accessDeniedPage("/access-denied");// 접근이 거부되었을 때 요청할 URI를 지정한다.
 			// 4. 예외처리 정책 설정 종료
 	}
